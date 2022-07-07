e4982e888427df96b5d7eeb9b6fb7bb15666e9dc
ivlkostya74 <ivlkostya@yandex.ru>
2022-07-08 00:20:38 +1000

    first commit

---
+++ src/Homwork/Main.java
@@ -0,0 +1,11 @@
+package Homwork;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.mult(5,4));
+        System.out.println(calc.pow(5,4));
+        System.out.println(calc.sum(7,8));
+    }
+}
